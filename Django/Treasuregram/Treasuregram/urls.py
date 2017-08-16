73e1eaa017d215959e82f5db2d2e4da62a62d376
stognienkovladislav <stognienkovv@gmail.com>
2017-08-16 13:15:07 +0300

    Django

--- Django/Treasuregram/Treasuregram/urls.py
+++ Django/Treasuregram/Treasuregram/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from main_app import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    #localhost/index
+
+    url(r'^index/', views.index)
 ]
