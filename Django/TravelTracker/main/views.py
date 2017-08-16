cd38f884eb15d81461b40dd72c0c783d320f1869
stognienkovladislav <stognienkovv@gmail.com>
2017-08-16 21:14:07 +0300

    Django

--- Django/TravelTracker/main/views.py
+++ Django/TravelTracker/main/views.py
@@ -1,9 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
 def home(request):
-    return HttpResponse("Welcome Home Travelers!")
+    context = {'locations' : locations}
+    return render(request, 'home.html', context)
+
+class Location:
+    def __init__(self, name, predators, num_restaurants, img_url):
+        self.name = name
+        self.predators = predators
+        self.num_restaurants = num_restaurants
+        self.img_url = img_url
+
+locations = [
+    Location("Fool's Falls, CO", 'Flash Floods', 0, 'http://courseware.codeschool.com/try_django/images/fools-falls.jpg'),
+    Location("Curly's Creek, NM", 'Rattle Snakes', 2, 'http://courseware.codeschool.com/try_django/images/curlys-creek.jpg'),
+    Location('The Delicate Arch, UT', 'Scorpions', 0, 'http://courseware.codeschool.com/try_django/images/delicate-arch.jpg')
+]
--- Django/Treasuregram/Treasuregram/urls.py
+++ Django/Treasuregram/Treasuregram/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from main_app import views
 urlpatterns = [
@@ -21,5 +21,6 @@ urlpatterns = [
 
     #localhost/index
 
-    url(r'^index/', views.index)
+    url(r'^',
+        include('main_app.urls'))
 ]
